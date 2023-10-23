948786cdd4f338672f73fde3df37fa0c0f1a687a
Andrii Lukashevych <andriy.lukashewwich@gmail.com>
2023-10-23 18:55:55 +0300

    fix: fix pgClient ssl configuration

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ const pgClient = new Pool({
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
+  ssl: { rejectUnauthorized: false },
 });
 
 pgClient.on("connect", (client) => {
